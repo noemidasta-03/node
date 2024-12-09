7851d109fe7272608a9ab6a53cff313d53442f8c
noemidasta-03 <noemi.dasta18@gmail.com>
2024-12-09 15:28:21 +0100

    exdb

--- node-ts/planets.ts
+++ node-ts/planets.ts
@@ -1,104 +1,84 @@
-import { Request,Response } from "express"
-import pgPromise from "pg-promise"
-import Joi from "joi"
+import { Request, Response } from "express";
+import pgPromise from "pg-promise";
+import Joi from "joi";
 
-const db=pgPromise()("postgres://postgres:postgress@localhost:5432/postgres")
+const db = pgPromise()(
+  "postgres://postgres:postgres@localhost:5432/planets_db"
+);
 
-const planetSchema= Joi.object({
+const planetSchema = Joi.object({
   id: Joi.number().integer().required().max(99).min(1),
-  name: Joi.string().required().max(10).min(4)
+  name: Joi.string().required().max(10).min(4),
+});
 
-})
-
-
-
-console.log(db)
+console.log(db);
 type Planet = {
-    id: number,
-    name: string,
-  };
+  id: number;
+  name: string;
+};
 
 type Planets = Planet[];
 
-let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
-
-const getAll= ((req:Request,res:Response)=>{
-    res.status(200).json(planets)
-})
-const getOneById=((req:Request,res:Response)=>{
-    const {id}= req.params
-
-    if( Joi.number().integer().required().max(99).min(1).validate(id).error){
-      res.status(400).json({msg:`error`})
-
-    }else{
-      const planet= planets.find((p)=>p.id === Number(id))
-
-
-      res.status(200).json(planet)
-
-    }
-   
-})
-const create=((req:Request,res:Response)=>{
-    const{id,name}= req.body
-    const newPlanet={id,name}
-    if(planetSchema.validate(newPlanet).error){
-      res.status(400).json({msg:`error`})
-      
-    }else{
-      planets= [...planets, newPlanet]
-
-      res.status(201).json({msg:`the planet was created`})
-      
-    }
-  
-})
-const updateById=((req:Request,res:Response)=>{
-    const id = Number(req.params?.id)
-    const {name}= req.body
-
-    const newPlanet = {id, name};
-    if(planetSchema.validate(newPlanet).error){
-      res.status(400).json({msg:`error`})
-    } else {
-      let isPlanetFound = false;
-      planets = planets.map((planet) => {
-        if(planet.id === newPlanet.id) {
-          isPlanetFound = true;
-          return newPlanet;
-        } else {
-          return planet;
-        }
-      })
-      res.status(200).json({msg: isPlanetFound ? `the planet was update` : `no planet found with given id: ${id}`})
-    }
-})
-const deleteById=((req:Request,res:Response)=>{
-    const{id}=req.params
-   
-
-    let isPlanetFound= false;
-
-    planets=planets.filter((planet)=>{
-      if(planet.id == Number(id)){
-        isPlanetFound= true;
-
-      }
-      return planet.id !== Number(id)
-
+const getAll = (req: Request, res: Response) => {
+  db.many(`SELECT * FROM planets`)
+    .then((planets: Planets) => {
+      res.status(200).json(planets);
     })
-    
-    res.status(200).json({msg: isPlanetFound ? `the planet was deleted` : `no planet found with given id: ${id}`})
-})
+    .catch((error) => {
+      res.status(404).json({ msg: "error " });
+    });
+};
+const getOneById = (req: Request, res: Response) => {
+  const { id } = req.params;
 
-export{getAll,getOneById,create,updateById,deleteById}
+  if (Joi.number().integer().required().max(99).min(1).validate(id).error) {
+    res.status(400).json({ msg: `error` });
+  } else {
+    db.one(`SELECT * FROM planets WHERE id=$1`, Number(id))
+      .then((planet?: Planet) => {
+        res.status(200).json(planet);
+      })
+      .catch((error) => {
+        res.status(404).json({ msg: "error " });
+      });
+  }
+};
+const create = (req: Request, res: Response) => {
+  const { name } = req.body;
+  const newPlanet = { id: 1, name };
+  if (planetSchema.validate(newPlanet).error) {
+    res.status(400).json({ msg: `validation error` });
+  } else {
+    db.query(`INSERT INTO planets (name) VALUES ($1)`, name).then(() => {
+      res.status(201).json({ msg: `the planet was created` });
+    });
+  }
+};
+const updateById = (req: Request, res: Response) => {
+  const id = Number(req.params?.id);
+  const { name } = req.body;
+
+  const newPlanet = { id, name };
+  if (planetSchema.validate(newPlanet).error) {
+    res.status(400).json({ msg: `error` });
+  } else {
+    db.query(`UPDATE planets SET name=$1 WHERE id=$2`, [name, id]).then(() => {
+      res.status(200).json({
+        msg: `the planet was update`,
+      });
+    });
+  }
+};
+const deleteById = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  let isPlanetFound = false;
+
+  db.query(`DELETE FROM planets WHERE id=$1`, id).then(() => {
+    res.status(200).json({
+      msg: `the planet was deleted`,
+    });
+  });
+};
+
+export { getAll, getOneById, create, updateById, deleteById };
--- node-ts/server.ts
+++ node-ts/server.ts
@@ -1,30 +1,23 @@
-import * as dotenv from "dotenv"
-dotenv.config()
-import express from "express"
-import morgan from "morgan"
-import {getAll,getOneById,create,updateById,deleteById} from "./planets"
-const app= express()
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import morgan from "morgan";
+import { getAll, getOneById, create, updateById, deleteById } from "./planets";
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
+app.get("/api/planets", getAll);
 
+app.get("/api/planets/:id", getOneById);
 
+app.post("/api/planets", create);
+app.put("/api/planets/:id", updateById);
 
+app.delete("/api/planets/:id", deleteById);
 
-app.get("/api/planets", getAll)
-
-
-app.get("/api/planets/:id",getOneById)
-
-app.post("/api/planets",create)
-app.put("/api/planets/:id",updateById)
-
-
-app.delete("/api/planets/:id",deleteById)
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`listening on:  http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`listening on:  http://localhost:${process.env.PORT}`);
+});
